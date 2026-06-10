81b18c4b1ee979e0ff69ffdf0d0f75cf7ddbe3c1
omar <202027471@std.sci.cu.edu.eg>
2026-06-10 15:52:29 +0300

    Initial commit

---
+++ src/main/java/com/example/aqsar/AqsarApplication.java
@@ -0,0 +1,13 @@
+package com.example.aqsar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqsarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqsarApplication.class, args);
+	}
+
+}
