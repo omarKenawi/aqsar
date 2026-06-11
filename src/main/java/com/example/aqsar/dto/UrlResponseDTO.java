6162d18dae5a66b5c4ac54f688d5d72cde8eb414
omar <202027471@std.sci.cu.edu.eg>
2026-06-11 14:56:48 +0300

    add shortKey on UrlResponseDTO

--- src/main/java/com/example/aqsar/dto/UrlResponseDTO.java
+++ src/main/java/com/example/aqsar/dto/UrlResponseDTO.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public record UrlResponseDTO(
         Long id,
         String originalUrl,
-        String shortUrl,
+        String shortKey,
         Long clickCount,
         LocalDateTime createdAt
 ) {}
