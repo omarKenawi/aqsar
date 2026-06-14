5a9b1897a5bd152f598d3401fa3d0a27b5d7e326
omar <202027471@std.sci.cu.edu.eg>
2026-06-14 06:45:45 +0300

    create ClickSyncJob every 30 sec

---
+++ src/main/java/com/example/aqsar/job/ClickSyncJob.java
@@ -0,0 +1,35 @@
+package com.example.aqsar.job;
+
+import com.example.aqsar.repository.ShortUrlRepository;
+import com.example.aqsar.service.ClickSyncService;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class ClickSyncJob {
+    private final RedisTemplate<String, String> redisTemplate;
+    private final ClickSyncService clickSyncService;
+
+    public ClickSyncJob(RedisTemplate<String, String> redisTemplate,
+                        ShortUrlRepository repository, ClickSyncService clickSyncService) {
+        this.redisTemplate = redisTemplate;
+        this.clickSyncService = clickSyncService;
+    }
+
+    @Scheduled(fixedRate = 30000)
+    public void syncClicks() {
+
+        Set<String> keys = redisTemplate.keys("click:*");
+
+        if (keys == null || keys.isEmpty()) return;
+
+        for (String key : keys) {
+            clickSyncService.flushKey(key);
+        }
+    }
+
+
+}
