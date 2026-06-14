016446a95fa167c5650eca522509e02cdd57c21f
omar <202027471@std.sci.cu.edu.eg>
2026-06-15 01:48:45 +0300

    implement double-buffering with atomic rename to avoid race conditions

--- src/main/java/com/example/aqsar/service/ClickSyncService.java
+++ src/main/java/com/example/aqsar/service/ClickSyncService.java
@@ -9,12 +9,9 @@ import org.springframework.stereotype.Service;
 public class ClickSyncService {
 
     private final ShortUrlRepository repository;
-    private final RedisTemplate<String, String> redisTemplate;
 
-    public ClickSyncService(ShortUrlRepository repository,
-                            RedisTemplate<String, String> redisTemplate) {
+    public ClickSyncService(ShortUrlRepository repository) {
         this.repository = repository;
-        this.redisTemplate = redisTemplate;
     }
     @Transactional
     public void flushKey(Object shortKeyObj, Object valueObj) {
